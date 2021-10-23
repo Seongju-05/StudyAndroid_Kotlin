e37cf1d637438567bb4220c542505eefe1c146f6
Sswmhskr06ksj <81690443+Sswmhskr06ksj@users.noreply.github.com>
2021-10-23 17:52:38 +0900

    2021-10-23

--- TiltSensor/app/src/main/java/com/hs/tiltsensor/MainActivity.kt
+++ TiltSensor/app/src/main/java/com/hs/tiltsensor/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hs.tiltsensor
 
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -8,6 +9,7 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private val sensorManager by lazy {
@@ -53,5 +55,22 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             Log.d("MainActivity", "onSensorChanged: x :" +
             "${event.values[0]}, y : ${event.values[1]}, z : ${event.values[2]}")
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 화면이 가로 모드로 고정되게 하기
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        // 화면 꺼지지 않게 하기
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        // 화면이 가로 모드로 고정되게 하기
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
